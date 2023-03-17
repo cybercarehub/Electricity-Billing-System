970f58c2929e7cd3538b99d35f86fd8bc08723bf
Cyber Hub <98678093+cybercarehub@users.noreply.github.com>
2023-03-17 22:27:52 +0530

    Uploaded By CyberHub 🐺

---
+++ src/electricitybill/ElectricityBill.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package electricitybill;
+
+/**
+ *
+ * @author UP
+ */
+public class ElectricityBill {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
